1d22a7f72f0243f2f2cb2121b146873e6db89b7e
leoloso <leo@getpop.org>
2023-06-08 08:29:49 +0000

    fixed cs (Renamed to disable the test)

--- tests/ModuleTestDisabled.php
+++ tests/ModuleTestDisabled.php
@@ -7,8 +7,8 @@ namespace PoPWPSchema\Pages;
 use PoP\ComponentModel\AbstractModuleTestCase;
 
 /**
- * Made abstract (and renamed) to disable the test
+ * Renamed to disable the test
  */
-abstract class ModuleTest_ extends AbstractModuleTestCase
+class ModuleTestDisabled extends AbstractModuleTestCase
 {
 }
